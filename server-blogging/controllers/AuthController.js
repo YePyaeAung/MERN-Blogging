3395cf05a1d090919c7e1bb7e1b168bf5b3e9672
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2024-05-09 15:25:41 +0700

    Register API test

--- server-blogging/controllers/AuthController.js
+++ server-blogging/controllers/AuthController.js
@@ -1,3 +1,8 @@
 export const login = (req, res) => {
-    res.send("Login Controller"); // 3
+    res.send("Login Controller");
+};
+
+export const register = (req, res) => {
+    console.log(req.body);
+    return res.json(req.body);
 };
--- server-blogging/routes/AuthRouter.js
+++ server-blogging/routes/AuthRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login } from "../controllers/AuthController.js";
+import { login, register } from "../controllers/AuthController.js";
 
 const router = express.Router();
 
-router.get("/login", login); // 2
+router.get("/login", login);
+router.post("/register", register);
 
 export default router;
