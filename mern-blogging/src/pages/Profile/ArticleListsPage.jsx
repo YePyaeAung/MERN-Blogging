aff22d9d9d0338bfdb0b5183bdf0df1869162802
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2024-05-27 18:40:04 +0700

    Page API and Data Binding Article List

--- mern-blogging/src/pages/Profile/ArticleListsPage.jsx
+++ mern-blogging/src/pages/Profile/ArticleListsPage.jsx
@@ -1,100 +1,92 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const ArticleListsPage = () => {
+    const [page, setPage] = useState(1);
+    const [articles, setArticles] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getArticles = async () => {
+        const { data } = await axios.get(`/auth/article?page=${page}`);
+        setArticles(data.data.articles);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        getArticles();
+    }, []);
+
     return (
         <>
-            <div className="row mt-5">
-                {/* article list */}
-                <div className="col-12 mt-3">
-                    <div className="card bg-dark d-flex flex-row">
-                        <img
-                            src="../../../public/images/1.webp"
-                            alt=""
-                            className="w-50"
-                        />
-                        <div className="ml-3 p-3">
-                            <h3 className="text-white">What is FullStack?</h3>
-                            <div className="d-flex justify-content-between">
-                                <div className="d-flex">
-                                    <span className="text-success mr-2">
-                                        <i className="bx bx-happy-heart-eyes" />
-                                    </span>
-                                    100
-                                </div>
-                                <div className="d-flex">
-                                    <span className="text-success mr-2">
-                                        <i className="bx bx-heart text-danger" />
-                                    </span>
-                                    100
-                                </div>
-                                <div className="d-flex">
-                                    <span className="text-success mr-2">
-                                        <i className="bx bx-message-square-dots text-primary" />
-                                    </span>
-                                    100
-                                </div>
-                            </div>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit....
-                            </p>
-                            <a
-                                href="#"
-                                className="btn btn-secondary float-right"
-                            >
-                                View
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                {/* article list */}
-                <div className="col-12 mt-3">
-                    <div className="card bg-dark d-flex flex-row">
-                        <img
-                            src="../../../public/images/1.webp"
-                            alt=""
-                            className="w-50"
-                        />
-                        <div className="ml-3 p-3">
-                            <h3 className="text-white">What is FullStack?</h3>
-                            <div className="d-flex justify-content-between">
-                                <div className="d-flex">
-                                    <span className="text-success mr-2">
-                                        <i className="bx bx-happy-heart-eyes" />
-                                    </span>
-                                    100
-                                </div>
-                                <div className="d-flex">
-                                    <span className="text-success mr-2">
-                                        <i className="bx bx-heart text-danger" />
-                                    </span>
-                                    100
-                                </div>
-                                <div className="d-flex">
-                                    <span className="text-success mr-2">
-                                        <i className="bx bx-message-square-dots text-primary" />
-                                    </span>
-                                    100
+            {isLoading ? (
+                <span>Loading...</span>
+            ) : (
+                <>
+                    <div className="row mt-5">
+                        {/* article list */}
+                        {articles.map(article => (
+                            <div key={article._id} className="col-12 mt-3">
+                                <div className="card bg-dark d-flex flex-row">
+                                    <img
+                                        src={`http://localhost:8888/images/${article.image}`}
+                                        alt=""
+                                        className="w-50"
+                                    />
+                                    <div className="ml-3 p-3">
+                                        <h3 className="text-white">
+                                            {article.title}
+                                        </h3>
+                                        <div className="d-flex justify-content-between">
+                                            <div className="d-flex">
+                                                <span className="text-success mr-2">
+                                                    <i className="bx bx-happy-heart-eyes" />
+                                                </span>
+                                                {article.view_count}
+                                            </div>
+                                            <div className="d-flex">
+                                                <span className="text-success mr-2">
+                                                    <i className="bx bx-heart text-danger" />
+                                                </span>
+                                                {article.like_count}
+                                            </div>
+                                            <div className="d-flex">
+                                                <span className="text-success mr-2">
+                                                    <i className="bx bx-message-square-dots text-primary" />
+                                                </span>
+                                                {article.comment_count}
+                                            </div>
+                                        </div>
+                                        <p>{article.description}</p>
+                                        <div className="d-flex justify-content-between">
+                                            <Link
+                                                to={"#"}
+                                                className="btn btn-secondary float-right"
+                                            >
+                                                View
+                                            </Link>
+                                            <Link
+                                                to={"#"}
+                                                className="btn btn-primary float-right"
+                                            >
+                                                Edit
+                                            </Link>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit....
-                            </p>
-                            <a
-                                href="#"
-                                className="btn btn-secondary float-right"
-                            >
-                                View
-                            </a>
+                        ))}
+                    </div>
+                    {/* load more button */}
+                    <div className="col-12 mt-5">
+                        <div className="d-flex justify-content-end">
+                            <button className="btn btn-primary">
+                                Load More
+                            </button>
                         </div>
                     </div>
-                </div>
-            </div>
-            {/* load more button */}
-            <div className="col-12 mt-5">
-                <div className="d-flex justify-content-end">
-                    <button className="btn btn-primary">Load More</button>
-                </div>
-            </div>
+                </>
+            )}
         </>
     );
 };
