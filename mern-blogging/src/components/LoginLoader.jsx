cf6342682d0d5310e0020eb3565a4a4364677df6
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2024-05-28 16:49:55 +0700

    Load More Button

---
+++ mern-blogging/src/components/LoginLoader.jsx
@@ -0,0 +1,14 @@
+const LoginLoader = () => {
+    return (
+        <div>
+            <span
+                className="spinner-border spinner-border-sm mr-3"
+                role="status"
+                aria-hidden="true"
+            ></span>
+            Logging In...
+        </div>
+    );
+};
+
+export default LoginLoader;
