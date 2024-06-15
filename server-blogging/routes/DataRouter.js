56a509a939028a55699a614bb450e48c4ec95960
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2024-06-15 17:37:39 +0700

    Get Latest Articles API | Get Most Trending Articles API | Get Most Love Articles API

--- server-blogging/routes/DataRouter.js
+++ server-blogging/routes/DataRouter.js
@@ -4,5 +4,8 @@ import DataController from "../controllers/DataController.js";
 const router = express.Router();
 
 router.get("/get-tags-langs", DataController.getTagsLangs);
+router.get("/get-latest-articles", DataController.getLatestArticles);
+router.get("/get-most-trending-articles", DataController.getMostTrendingArticles);
+router.get("/get-most-love-articles", DataController.getMostLoveArticles);
 
 export default router;
