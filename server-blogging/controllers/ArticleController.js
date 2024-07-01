a793bbc64682579e05ded6dd30bcbdaebc20b77b
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2024-07-02 06:24:08 +0700

    Change Article Details Page (Auth to Not-Auth) | Creating Comment and Increasing Comment Count

--- server-blogging/controllers/ArticleController.js
+++ server-blogging/controllers/ArticleController.js
@@ -49,22 +49,6 @@ export const all = async (req, res) => {
     }
 };
 
-export const show = async (req, res) => {
-    try {
-        const { slug } = req.params;
-        await ArticleModel.findOneAndUpdate(
-            { slug },
-            {
-                $inc: { view_count: 1 },
-            }
-        );
-        const article = await ArticleModel.findOne({ slug });
-        res.json(successJson("Get Single Article Successfully!", article));
-    } catch (error) {
-        res.json(errorJson("Something went wrong!", null));
-    }
-};
-
 export const store = async (req, res) => {
     try {
         const { files, body } = req;
--- server-blogging/routes/DataRouter.js
+++ server-blogging/routes/DataRouter.js
@@ -5,8 +5,12 @@ const router = express.Router();
 
 router.get("/get-tags-langs", DataController.getTagsLangs);
 router.get("/get-latest-articles", DataController.getLatestArticles);
-router.get("/get-most-trending-articles", DataController.getMostTrendingArticles);
+router.get(
+    "/get-most-trending-articles",
+    DataController.getMostTrendingArticles
+);
 router.get("/get-most-love-articles", DataController.getMostLoveArticles);
 router.get("/get-all-articles", DataController.getAllArticles);
+router.get("/article/:slug", DataController.getArticleDetails);
 
 export default router;
